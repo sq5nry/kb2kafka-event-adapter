85b9ecf70f27a14a569710f85b926d79a8ff1197
Pawel Stobinski <pawel.stobinski@computaris.com>
2021-09-10 03:27:46 +0200

    async prod

--- cmd/ratings/main.go
+++ cmd/ratings/main.go
@@ -2,50 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/Shopify/sarama"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"kb2kafka-event-adapter/config"
-	//"kb2kafka-event-adapter/dependency"
-	//"kb2kafka-event-adapter/domain/interactor"
 	"log"
 	"net/http"
 )
-
-func main() {
-	//kafkaProperties := config.ReadKafkaConfigurationFromFile()
-	listenerConfig := config.GetListenerConfig()
-
-	http.HandleFunc(listenerConfig.EndpointPath, onPost)
-
-	log.Println("DEBUG: starting listener")
-	if err := http.ListenAndServe(listenerConfig.ListenerAddress, nil); err != nil {
-		log.Fatal(err)
-	}
-	//db, err := dependency.NewPostgresConnection()
-	//
-	//if err != nil {
-	//	fmt.Printf("%s\n", err.Error())
-	//	return
-	//} else {
-	//	fmt.Println("Database connection was initialized.")
-	//	defer dependency.Close(db)
-	//}
-
-	//ratingInteractor := interactor.NewRatingInteractor(
-	//	dependency.NewRatingRepository(db),
-	//	dependency.NewRatingConsumer(kafkaProperties),
-	//	dependency.NewIdGenerator(),
-	//)
-
-	//fmt.Println("Start receiving from Kafka")
-	//for {
-	//	err := ratingInteractor.GetIncomingMessage()
-	//
-	//	if err != nil {
-	//		fmt.Printf("error from ratingInteractor: [%s]\n", err)
-	//	}
-	//}
-}
 
 type KBEvent struct {
 	EventType  string `json:"eventType"`  //type of event (as defined by the ExtBusEventType enum)
@@ -61,41 +24,52 @@ type RatingMessage struct {
 	Value    int8   `json:"value"`
 }
 
+var kafkaConfig = config.ReadKafkaConfigurationFromFile()
+var kafkaProducer kafka.Producer = createProducer(&kafkaConfig)
+
+func createProducer(config *config.KafkaConfiguration) kafka.Producer {
+	kafkaProducer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": config.BootstrapServer,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return *kafkaProducer
+}
+
+func main() {
+	listenerConfig := config.GetListenerConfig()
+
+	http.HandleFunc(listenerConfig.EndpointPath, onPost)
+
+	log.Println("DEBUG: starting listener")
+	if err := http.ListenAndServe(listenerConfig.ListenerAddress, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func onPost(writer http.ResponseWriter, request *http.Request) {
-	//TODO single condition
 	switch request.Method {
 	case "POST":
-		var event KBEvent
-		err := json.NewDecoder(request.Body).Decode(&event)
+		var kbEvent KBEvent
+		err := json.NewDecoder(request.Body).Decode(&kbEvent)
 		if err != nil {
-			//TODO provide a message
-			http.Error(writer, err.Error(), http.StatusBadRequest)
+			handleError(writer, http.StatusBadRequest, "decoding error", err)
 			return
 		}
-		//TODO handle error
-		//eventBytes, err := json.Marshal(event)
-		log.Printf("DEBUG: event from KB received: %#v\n", event)
 
-		var ratingMsg RatingMessage;
-		ratingMsg.Id = event.AccountId	//TODO account or object id?
-		ratingMsg.RecipeId = event.AccountId	//string(eventBytes)	//TODO should be in "value", putting it here for now
-		ratingMsg.Value = 123
-
+		log.Printf("DEBUG: kbEvent from KB received: %#v\n", kbEvent)
+		ratingMsg := createRatingFromKbEvent(kbEvent)
 		ratingMsgBytes, err := json.Marshal(ratingMsg)
 		if err == nil {
-			//TODO async
-			//TODO topic from config
 			log.Printf("DEBUG: pushing KB request to kafka, id=%s, from=%s", ratingMsg.Id, request.RemoteAddr)
-			err := pushKbEventToQueue("test-topic", ratingMsgBytes, ratingMsg.Id)
+			err = dispatchToKafka(ratingMsgBytes, ratingMsg.Id, &kafkaProducer)
 			if err != nil {
-				var msg = fmt.Sprintf("could not push to kafka: %v", err)
-				log.Println("ERROR: " + msg)
-				http.Error(writer, msg, http.StatusInternalServerError)
+				handleError(writer, http.StatusInternalServerError, "could not push to kafka", err)
 			}
 		} else {
-			var msg = fmt.Sprintf("could not create a rating message: %v", err)
-			log.Println("ERROR: " + msg)
-			http.Error(writer, msg, http.StatusInternalServerError)
+			handleError(writer, http.StatusInternalServerError, "could not create a rating message", err)
 		}
 
 	default:
@@ -105,36 +79,40 @@ func onPost(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func connectProducer(brokersUrl []string) (sarama.SyncProducer,error) {
-	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	config.Producer.Retry.Max = 5
-	// NewSyncProducer creates a new SyncProducer using the given broker addresses and configuration.
-	conn, err := sarama.NewSyncProducer(brokersUrl, config)
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
+func createRatingFromKbEvent(event KBEvent) RatingMessage {
+	var ratingMsg RatingMessage
+	ratingMsg.Id = event.AccountId       //TODO assuming KB.AccountId
+	ratingMsg.RecipeId = event.AccountId //TODO assuming KB.AccountId
+	ratingMsg.Value = 123                //TODO KB data should go here
+	return ratingMsg
 }
 
-func pushKbEventToQueue(topic string, message []byte, key string) error {
-	brokerUrl := []string{"localhost:9092"}	//TODO config
-	producer, err := connectProducer(brokerUrl)
+func handleError(writer http.ResponseWriter, code int, description string, err error) {
+	var msg = fmt.Sprintf(description+": %v", err)
+	log.Println("ERROR: " + msg)
+	http.Error(writer, msg, code)
+}
+
+func dispatchToKafka(message []byte, key string, producer *kafka.Producer) error {
+	deliveryChan := make(chan kafka.Event)
+	err := producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &kafkaConfig.ConsumerTopicName, Partition: kafka.PartitionAny},
+		Value:          message,
+		Key: 			[]byte(key),
+	}, deliveryChan)
 	if err != nil {
+		log.Printf("ERROR: creating a message for delivery failed: %v\n", err)
 		return err
-	}
-	defer producer.Close()
-	msg := &sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.StringEncoder(message),
-		Key: sarama.StringEncoder(key),
 	}
 
-	partition, offset, err := producer.SendMessage(msg)
-	if err != nil {
-		return err
+	event := <-deliveryChan
+	ack := event.(*kafka.Message)
+	if ack.TopicPartition.Error != nil {
+		log.Printf("ERROR: delivery failed: %v\n", ack.TopicPartition.Error)
+		return errors.New("delivery failed")
+	} else {
+		log.Printf("DEBUG: delivered message [%s] with key [%s] to topic %s [%d] at offset %v\n",
+			message, key, *ack.TopicPartition.Topic, ack.TopicPartition.Partition, ack.TopicPartition.Offset)
+		return nil
 	}
-	log.Printf("DEBUG: Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
-	return nil
 }
