fc31380016a44e49d030bc6c393050453b4b7568
Pawel Stobinski <pawel.stobinski@computaris.com>
2021-09-15 18:24:41 +0200

    config in init glog logger

--- cmd/ratings/main.go
+++ cmd/ratings/main.go
@@ -3,65 +3,93 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	kafkaConf "kb2kafka-event-adapter/config/kafka"
-	listenerConf "kb2kafka-event-adapter/config/listener"
+	"github.com/golang/glog"
+	kafkaCfg "kb2kafka-event-adapter/config/kafka"
+	listenerCfg "kb2kafka-event-adapter/config/listener"
 	"kb2kafka-event-adapter/domain/broker"
 	"kb2kafka-event-adapter/domain/entity"
-	"log" //TODO debug tracing instead of always evaluated printf
 	"math/rand"
 	"net/http"
 	"os"
 )
 
-var kafkaConfig, kafkaConfigErr = kafkaConf.GetKafkaConfiguration()      //TODO not global, handle error
-var listenerConfig, listenerConfigErr = listenerConf.GetListenerConfig() //TODO not global, handle error
+var kafkaConfig *kafkaCfg.KafkaConfiguration
+var listenerConfig *listenerCfg.ListenerConfiguration
+var kafkaProducer *kafka.Producer
 
-var kafkaProducer = createProducer(kafkaConfig)
+func init() {
+	initializeGlog()
+
+	var err error
+	kafkaConfig, err = kafkaCfg.GetKafkaConfiguration()
+	if err != nil {
+		glog.Fatalf("error in kafka configuration: %#v", err)
+	}
+
+	listenerConfig, err = listenerCfg.GetListenerConfig()
+	if err != nil {
+		glog.Fatalf("error in listener configuration: %#v", err)
+	}
+
+	kafkaProducer = createProducer(kafkaConfig)
+}
+
+func initializeGlog() {
+	flag.Parse()	//workaround for glog "logging before flag.Parse" message
+}
 
 //TODO readiness & liveness probing
 func main() {
-	ensureConfigurationLoaded()
 	http.HandleFunc(listenerConfig.EndpointPath, onHttpReq)
 
-	log.Println("DEBUG: starting listener")
+	glog.Info("starting listener")
 	if err := http.ListenAndServe(listenerConfig.ListenerAddress, nil); err != nil {
-		log.Fatal("ERROR: " + err.Error())
-	}
-}
-
-func ensureConfigurationLoaded() {
-	if kafkaConfigErr != nil {
-		log.Fatal("ERROR: " + kafkaConfigErr.Error())
-	}
-	if listenerConfigErr != nil {
-		log.Fatal("ERROR: " + listenerConfigErr.Error())
+		glog.Exit(err.Error())
 	}
 }
 
 func onHttpReq(writer http.ResponseWriter, request *http.Request) {
-	if request.Method != http.MethodPost {
-		const msg = "only POST supported"
-		http.Error(writer, msg, http.StatusMethodNotAllowed)
-		log.Printf("WARN: %s, from=%s", msg, request.RemoteAddr)
+	if ensureHttpMethod(writer, request) {
+		return
 	}
 
+	kbEvent, done := extractKbData(writer, request)
+	if done {
+		return
+	}
+
+	handleKbEvent(writer, kbEvent)
+}
+
+func extractKbData(writer http.ResponseWriter, request *http.Request) (entity.KBEvent, bool) {
 	var kbEvent entity.KBEvent
 	err := json.NewDecoder(request.Body).Decode(&kbEvent)
 	if err != nil {
 		handleError(writer, http.StatusBadRequest, "decoding error", err)
-		return
+		return entity.KBEvent{}, true
 	}
-	log.Printf("DEBUG: kbEvent from KB received: %#v\n", kbEvent)
-	handleKbEvent(writer, request, kbEvent)
+	glog.Info("KbEvent received", kbEvent)
+	return kbEvent, false
 }
 
-func handleKbEvent(writer http.ResponseWriter, request *http.Request, kbEvent entity.KBEvent) {
+func ensureHttpMethod(writer http.ResponseWriter, request *http.Request) bool {
+	if request.Method != http.MethodPost {
+		const msg = "only POST supported"
+		http.Error(writer, msg, http.StatusMethodNotAllowed)
+		glog.Warning(msg, ", request from=", request.RemoteAddr)
+		return true
+	}
+	return false
+}
+
+func handleKbEvent(writer http.ResponseWriter, kbEvent entity.KBEvent) {
 	ratingMsg := createRatingFromKbEvent(kbEvent)
 	ratingMsgBytes, err := json.Marshal(ratingMsg)
 	if err == nil {
-		log.Printf("DEBUG: pushing KB request to kafka, id=%s, from=%s", ratingMsg.Id, request.RemoteAddr)
+		glog.Infof("pushing KB request to kafka, id=%s", ratingMsg.Id)
 		err = dispatchToKafka(ratingMsgBytes, ratingMsg.Id, kafkaProducer)
 		if err != nil {
 			handleError(writer, http.StatusInternalServerError, "could not push to kafka", err)
@@ -76,14 +104,13 @@ func createRatingFromKbEvent(event entity.KBEvent) broker.RatingMessage {
 	ratingMsg.Id = event.AccountId       //TODO assuming KB.AccountId
 	ratingMsg.RecipeId = event.AccountId //TODO assuming KB.AccountId
 	ratingMsg.Value = int8(rand.Int())   //TODO field needed?
-	ratingMsg.Body = event.MetaData;
+	ratingMsg.Body = event.MetaData
 	return ratingMsg
 }
 
 func handleError(writer http.ResponseWriter, code int, description string, err error) {
-	var msg = fmt.Sprintf(description+": %v", err)
-	log.Printf("ERROR: %s\n", msg)
-	http.Error(writer, msg, code)
+	glog.Error(err.Error())
+	http.Error(writer, fmt.Sprintf(description+": %v", err), code)
 }
 
 func dispatchToKafka(message []byte, key string, producer *kafka.Producer) error {
@@ -94,26 +121,26 @@ func dispatchToKafka(message []byte, key string, producer *kafka.Producer) error
 		Key:            []byte(key),
 	}, deliveryChan)
 	if err != nil {
-		log.Printf("ERROR: creating a message for delivery failed: %v\n", err)
+		glog.Warningf("creating a message for delivery failed: %#v\n", err)
 		return err
 	}
 
 	event := <-deliveryChan
 	ack := event.(*kafka.Message)
 	if ack.TopicPartition.Error != nil {
-		log.Printf("ERROR: delivery failed: %v\n", ack.TopicPartition.Error)
+		glog.Warning("delivery failed: ", ack.TopicPartition.Error)
 		return errors.New("delivery failed")
 	} else {
-		log.Printf("DEBUG: delivered message [%s] with key [%s] to topic %s [%d] at offset %v\n",
+		glog.Infof("delivered message [%s] with key [%s] to topic %s [%d] at offset %v\n",
 			message, key, *ack.TopicPartition.Topic, ack.TopicPartition.Partition, ack.TopicPartition.Offset)
 		return nil
 	}
 }
 
-func createProducer(config *kafkaConf.KafkaConfiguration) *kafka.Producer {
+func createProducer(config *kafkaCfg.KafkaConfiguration) *kafka.Producer {
 	host, err := os.Hostname()
 	if err != nil {
-		log.Fatal("ERROR: " + err.Error())
+		glog.Fatal(err.Error())
 	}
 
 	kafkaProducer, err := kafka.NewProducer(&kafka.ConfigMap{
@@ -121,7 +148,7 @@ func createProducer(config *kafkaConf.KafkaConfiguration) *kafka.Producer {
 		"client.id":         host,
 	})
 	if err != nil {
-		log.Fatal(err)
+		glog.Fatal(err)
 	}
 	return kafkaProducer
 }
